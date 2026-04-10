232efac487a1887fa070c0dcbab65cce933f3cbb
lenniii <dennisalessandrini@gmail.com>
2026-04-10 19:30:39 +0200

    feat: add event widget data model

---
+++ src/features/events/constants.ts
@@ -0,0 +1,7 @@
+import type { EventTheme } from "./types";
+
+export const EVENTS_BASE_URL = "https://events.roundtable.it";
+export const DEFAULT_EVENT_IMAGE =
+  "https://events.roundtable.it/static/events_app/images/og-default.png";
+
+export const DEFAULT_THEME: EventTheme = "dark";
